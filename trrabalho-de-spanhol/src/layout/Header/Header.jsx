034ee4d1b6f28bc723c1c3053ccd6b638d617b5a
tiago-burlamaque <tiagoburlamaque79@gmail.com>
2025-10-07 11:42:10 -0300

    atualizar mais tarde

---
+++ trrabalho-de-spanhol/src/layout/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Header() {
+    return (
+    <header className='flex items-center justify-between p-4 bg-gray-200'>
+        <nav className='flex gap-4'>
+            <NavLink to='/' end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
+                Home
+            </NavLink>
+            <NavLink to="/sobre" end className={({ isActive }) => isActive ? "text-blue-600 font-bold" : "text-gray-800"}>
+                Sobre
+            </NavLink>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header
