ade55eb04c189c6a610368f13dac3c4c7d069220
TIAGO BURLAMAQUE SOUSA FILHO <tiago_sousa-filho@estudante.sesisenai.org.br>
2025-10-08 20:14:01 -0300

    atualizado

---
+++ trabalho-de-espanhol/src/layout/Rodape/Rodape.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Rodape() {
+  return (
+    <div className='fixed bottom-0 left-0 w-full bg-gray-600 text-white p-4 flex justify-between'>
+        <div className="text-start">
+      <h1 className='text-black text-xl'>
+        Integrantes:
+      </h1>
+      <ul>
+        <li>
+            Gabriel
+        </li>
+        <li>
+            Arthur viléla
+        </li>
+        <li>
+            Livia
+        </li>
+        <li>
+            Tiago Burlamaque
+        </li>
+      </ul>
+        </div>
+        <div className="text-center">
+            <h1 className='text-black text-xl'>fontes:</h1>
+            <a href="https://www.britannica.com/place/Equatorial-Guinea/Cultural-life">Britannica</a>
+        </div>
+        <div className=''>
+            <h1>
+                                                                                                                               
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Rodape
--- trabalho-de-espanhol/src/pages/Home/Home.jsx
+++ trabalho-de-espanhol/src/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import Rodape from '../../layout/Rodape/Rodape'
 
 function Home() {
   return (
     <div className='flext-col '>
-      <h1 className='text-white text-4xl pl-4 pt-4 mb-4'>Trabalho de Espanhol</h1>
+      <h1 className='text-white text-4xl pl-4 pt-4 mb-4'>Guiné Equatorial</h1>
       <hr className='ml-5 mr-4 text-white'/> 
+      <div className="flex">
+      <p className='text-clip m-5 text-white text-3xl text-center'>
+      Apesar de um verniz de cultura espanhola e de religião católica romana mais denso em Bioko do que no continente, muitos guineenses equatoriais vivem de acordo com costumes ancestrais, que passaram por um renascimento desde a independência. Entre os fang do continente, a bruxaria, a música tradicional (na qual se utilizam a harpa fang, o xilofone, os grandes tambores e a trombeta de madeira) e a narração de histórias sobrevivem. Entre os agricultores bubi de Bioko, alguns costumes ancestrais também são seguidos.
+      </p>
+      <img src="../public/guineEquatorial.png" alt="" className='p-4 w-1000'/>
+      </div>
       
-    </div>
+      <footer>
+        <Rodape />
+      </footer>
+    </div>  
   )
 }
 
