08c1e22d0f55a237b81fab0ab680a6573df4c488
Tiago-burlamaque <tiagoburlamaque79@gmail.com>
2025-10-08 13:48:29 -0300

    adicionando e removendo pastas

---
+++ trabalho-de-espanhol/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flext-col '>
+      <h1 className='text-white text-4xl pl-4 pt-4 mb-4'>Trabalho de Espanhol</h1>
+      <hr className='ml-5 mr-4 text-white'/> 
+      
+    </div>
+  )
+}
+
+export default Home
